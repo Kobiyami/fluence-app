3ede3637e533b9d806efae00d0e1e5e62fbb07e2
Guillaume Fournier <ton-email-git@exemple.com>
2026-02-23 00:21:07 +0100

    first seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Student.destroy_all
+Text.destroy_all
+Session.destroy_all
+
+puts "Creating students..."
+
+students = [
+  { first_name: "Alice", last_name: "Martin", code: "1234" },
+  { first_name: "Benoît", last_name: "Durand", code: "2345" },
+  { first_name: "Chloé", last_name: "Petit", code: "3456" },
+  { first_name: "David", last_name: "Leroy", code: "4567" }
+]
+
+students.each do |data|
+  Student.create!(data)
+end
+
+puts "Students created!"
