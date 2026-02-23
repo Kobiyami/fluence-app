3deecb6a2d1fcf8eab190e81d98bf5b43cfe49f7
Guillaume Fournier <ton-email-git@exemple.com>
2026-02-23 09:49:39 +0100

    sécurisation division par 0

--- app/models/session.rb
+++ app/models/session.rb
@@ -6,9 +6,12 @@ class Session < ApplicationRecord
 
   private
 
-  def compute_score
-    return if duration_seconds.blank? || text.blank?
+  def compute_score!
+  return if aborted?
+  return if duration_seconds.to_i <= 0
+  return if text.blank?
 
-    self.score_wpm = (text.word_count / (duration_seconds.to_f / 60)).round
+  minutes = duration_seconds.to_f / 60
+  self.score_wpm = (text.word_count / minutes).round
   end
 end
