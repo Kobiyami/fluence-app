acd9776932f9d594000ad7dcdef79a77b9c7f2f0
Guillaume Fournier <ton-email-git@exemple.com>
2026-02-23 14:34:28 +0100

    add helper name -> firstname lastname

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,8 @@ class Student < ApplicationRecord
   has_many :sessions, dependent: :destroy
 
   validates :first_name, :last_name, :code, presence: true
+
+def name
+  "#{first_name} #{last_name}"
+end
 end
